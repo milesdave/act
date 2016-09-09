da6751fe59d6a697923bbfc794d51f5bb610760f
Miles <davis.g.miles@gmail.com>
2016-09-09 10:52:46 +0930

    check for source files by file extension

--- act.py
+++ act.py
@@ -1,1 +1,21 @@
 #!/bin/python
+
+import os
+
+# checks if the file is a src file by extension
+def chkIfSrc(filename):
+	extensions = [".c", ".cpp", ".cs", ".h", ".hpp", "java"]
+	for ext in extensions:
+		if ext in filename:
+			return True
+	return False
+
+# total TODOs found
+total = 0
+
+# go through all the files in the current dir
+for filename in os.listdir(os.getcwd()):
+	print(filename + ": " + str(chkIfSrc(filename)))
+
+# final output
+print("\nTotal: " + str(total))
