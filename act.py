1c56ab5badd4c2a86bc45ccf06d8c1ade0e75886
Miles <davis.g.miles@gmail.com>
2016-09-09 10:59:50 +0930

    opening file and looking for TODOs

--- act.py
+++ act.py
@@ -4,7 +4,7 @@ import os
 
 # checks if the file is a src file by extension
 def chkIfSrc(filename):
-	extensions = [".c", ".cpp", ".cs", ".h", ".hpp", "java"]
+	extensions = [".c", ".cpp", ".cs", ".h", ".hpp", ".java"]
 	for ext in extensions:
 		if ext in filename:
 			return True
@@ -15,7 +15,20 @@ total = 0
 
 # go through all the files in the current dir
 for filename in os.listdir(os.getcwd()):
-	print(filename + ": " + str(chkIfSrc(filename)))
+	# open the file if it's a src file
+	if chkIfSrc(filename):
+		f = open(filename, "r")
+		lineNum = 1
+		print(filename + ":")
+
+		# scan for TODOs
+		for line in f:
+			if "TODO" in line:
+				print("  " + line, end="")
+				total += 1
+			lineNum += 1
+
+		f.close()
 
 # final output
 print("\nTotal: " + str(total))
