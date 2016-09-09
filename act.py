15fd5543b87c9c02b928e3658e0c78bb5fa769b7
Miles <davis.g.miles@gmail.com>
2016-09-09 11:23:47 +0930

    better way to print output

--- act.py
+++ act.py
@@ -11,11 +11,32 @@ def chkIfSrc(filename):
 	return False
 
 # formats a line with a TODO in it
-def formatter(line):
+def formatter(lineNum, line):
 	line = line.replace("//", "")
 	line = line.replace("TODO", "")
 	line = line.strip()
-	return "\"" + line + "\""
+	return str(lineNum) + ": \"" + line + "\""
+
+# scans a file for TODOs and prints them
+def scanFile(f):
+	total = 0
+	lineNum = 1
+	todoList = []
+
+	# check each line for a TODO
+	for line in f:
+		if "TODO" in line:
+			todoList.append(formatter(lineNum, line))
+			total += 1
+		lineNum += 1
+
+	# print todoList
+	if todoList:
+		print(f.name + ":")
+		for todo in todoList:
+			print("  " + todo)
+
+	return total
 
 # total TODOs found
 total = 0
@@ -25,17 +46,7 @@ for filename in os.listdir(os.getcwd()):
 	# open the file if it's a src file
 	if chkIfSrc(filename):
 		f = open(filename, "r")
-		lineNum = 1
-		print(filename + ":")
-
-		# scan for TODOs
-		for line in f:
-			if "TODO" in line:
-				print("  " + str(lineNum) + ": "+ formatter(line))
-				total += 1
-			lineNum += 1
-
+		total += scanFile(f)
 		f.close()
 
-# final output
 print("\nTotal: " + str(total))
