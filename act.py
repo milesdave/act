3e7dad07793756d9ef1da1ea51ca5697cf324af9
Miles Davis <davis.g.miles@gmail.com>
2017-06-08 22:05:17 +0930

    added colour and fixed issue with no TODOs

--- act.py
+++ act.py
@@ -9,7 +9,8 @@ def main():
 	try:
 		opts, args, = getopt.getopt(sys.argv[1:], "r")
 	except getopt.GetoptError:
-		print("Usage: act [-r]")
+		print("Usage: act [-r]\n"
+			"  -r        search directories recursively")
 		sys.exit(1)
 	for opt, arg in opts:
 		if opt in "-r":
@@ -25,8 +26,7 @@ def read_dir(path, recursive):
 		if recursive and os.path.isdir(name):
 			total += read_dir(name, recursive)
 		elif check_if_src(name):
-			print(name + ":")
-			total += read_file(path + "/" + name)
+			total += read_file(path + "/", name)
 	return total
 
 # checks if the given file is a source file by extension
@@ -38,11 +38,11 @@ def check_if_src(name):
 	return False
 
 # scans a file for TODO tags and prints them
-def read_file(name):
+def read_file(path, name):
 	total = 0
 	line_num = 1
 	todo_list = []
-	src = open(name, "r")
+	src = open(path + name, "r")
 	# check each line for a TODO
 	for line in src:
 		if check_todo(line):
@@ -51,6 +51,7 @@ def read_file(name):
 		line_num += 1
 	# print TODO list
 	if todo_list:
+		print(name + ":")
 		for todo in todo_list:
 			print(todo)
 		print("")
@@ -69,8 +70,12 @@ def check_todo(line):
 # formats a line with a TODO tag in it
 def formatter(lineNum, line):
 	line = line.strip()
+	# add colour around TODO
+	tmp = line.lower()
+	index = tmp.find("todo")
+	line = line[:index] + "\x1b[1;31m" + line[index:index + 4] + "\x1b[0m" + line[index + 4:]
 	# truncate line if it's too long
-	line = (line[:50] + "...") if len(line) > 50 else line
+	line = (line[:70] + "...") if len(line) > 70 else line
 	return str(lineNum).rjust(4) + ": " + line
 
 if __name__ == "__main__":
