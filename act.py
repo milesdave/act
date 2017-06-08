83bd4c907ef64aadb5d7fa6d52454842eb84370d
Miles Davis <davis.g.miles@gmail.com>
2017-06-08 21:27:17 +0930

    simplified formatter()

--- act.py
+++ act.py
@@ -10,7 +10,7 @@ def main():
 # recursively (or not) search the given directory for source files
 # returns total amount of TODO comments found
 def read_dir(path, recursive):
-	total = 0 # total TODO comments
+	total = 0
 	for name in os.listdir(path):
 		if recursive and os.path.isdir(name):
 			total += read_dir(name, recursive)
@@ -35,15 +35,14 @@ def read_file(name):
 	src = open(name, "r")
 	# check each line for a TODO
 	for line in src:
-		line = line.strip()
 		if check_todo(line):
-			todo_list.append(line)
+			todo_list.append(formatter(line_num, line))
 			total += 1
 		line_num += 1
 	# print TODO list
 	if todo_list:
 		for todo in todo_list:
-			print("  " + todo)
+			print(todo)
 		print("")
 	src.close
 	return total
@@ -57,22 +56,12 @@ def check_todo(line):
 		return True
 	return False
 
-# formats a line with a todo tag in it
+# formats a line with a TODO tag in it
 def formatter(lineNum, line):
-	# remove actual line of code if there is any
-	tmp = line.lower()
-	todoStart = tmp.index("todo")
-	line = line[todoStart:]
-	# remove comment stuff
-	toReplace = ["//", "/*", "*/", "TODO:", "todo:", "TODO", "todo"]
-	for rep in toReplace:
-		line = line.replace(rep, "")
 	line = line.strip()
 	# truncate line if it's too long
 	line = (line[:50] + "...") if len(line) > 50 else line
-	# add "???" if it's empty now
-	line = line if line else "???"
-	return str(lineNum) + ": \"" + line + "\""
+	return str(lineNum).rjust(4) + ": " + line
 
 if __name__ == "__main__":
 	main()
